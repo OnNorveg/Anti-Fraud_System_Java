55190e9416fb864bbcaed0fc952e71d0544ef905
OnNorveg <myway2984@gmail.com>
2025-12-09 22:33:30 +0100

    Step 2

--- Anti-Fraud System/task/src/antifraud/AntiFraudController.java
+++ Anti-Fraud System/task/src/antifraud/AntiFraudController.java
@@ -2,12 +2,21 @@ package antifraud;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AntiFraudController {
+
+    private final AppUserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AntiFraudController(AppUserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PostMapping("/api/antifraud/transaction")
     public ResponseEntity<TransactionResult> validateTransaction(@RequestBody TransactionRequest request){
@@ -21,4 +30,18 @@ public class AntiFraudController {
             return new ResponseEntity<>(new TransactionResult("PROHIBITED"), HttpStatus.OK);
         }
     }
+
+    @PostMapping(path = "/api/auth/user")
+    public String register(@RequestBody RegistrationRequest request) {
+        var user = new AppUser();
+        user.setUsername(request.username());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setAuthority(request.authority());
+
+        repository.save(user);
+
+        return "New user successfully registered";
+    }
+
+    record RegistrationRequest(String username, String password, String authority) { }
 }

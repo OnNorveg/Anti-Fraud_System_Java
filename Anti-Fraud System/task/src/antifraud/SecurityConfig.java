75e9b32121487f2f983c7f059d5fa87f91b4a613
OnNorveg <myway2984@gmail.com>
2025-12-09 22:33:30 +0100

    Step 2

---
+++ Anti-Fraud System/task/src/antifraud/SecurityConfig.java
@@ -0,0 +1,35 @@
+package antifraud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable)                           // For modifying requests via Postman
+                .exceptionHandling(handing -> handing
+                        .authenticationEntryPoint(restAuthenticationEntryPoint) // Handles auth error
+                )
+                .headers(headers -> headers.frameOptions().disable())           // for Postman, the H2 console
+                .authorizeHttpRequests(requests -> requests                     // manage access
+                                .requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
+                                .requestMatchers("/actuator/shutdown").permitAll()      // needs to run test
+                        // other matchers
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // no session
+                )
+                // other configurations
+                .build();
+    }
+}
